8acbe75f3e66f424a87e5fc9747190dbd4482bf2
Angela Jiang <to.ajiang@gmail.com>
2019-05-13 21:32:58 -0400

    Update cifar100 to relative-cubed

--- scripts/neurips19/run_cifar100_fast_gradual.sh
+++ scripts/neurips19/run_cifar100_fast_gradual.sh
@@ -2,6 +2,7 @@ expname=$1
 SAMPLING_MIN=$2
 LR=$3
 
+
 set -x
 
 ulimit -n 2048
@@ -12,9 +13,11 @@ NET="wideresnet"
 BATCH_SIZE=64
 DECAY=0.0005
 SEED=1337
-NUM_TRIALS=3
+NUM_TRIALS=1
+PROB_STRATEGY="relative-cubed"
+LOSS="cross"
 
-EXP_NAME=$EXP_PREFIX
+EXP_NAME=$EXP_PREFIX"_"$PROB_STRATEGY"_"$LOSS
 
 mkdir "/proj/BigLearning/ahjiang/output/cifar100/"
 OUTPUT_DIR="/proj/BigLearning/ahjiang/output/cifar100/"$EXP_NAME
@@ -27,6 +30,7 @@ do
 
   echo $OUTPUT_DIR/$OUTPUT_FILE
 
+  # WARNING: Many of these options are hardcoded in lib/SelectiveBackpropper.py
   time python -u train.py \
      --seed=$SEED \
      --batch_size=$BATCH_SIZE \
@@ -35,7 +39,7 @@ do
      --data_augmentation \
      --cutout \
      --length=8 \
-     --epochs=2000 \
+     --epochs=650 \
      --output_dir=$OUTPUT_DIR \
      --sb \
      --lr_sched=$LR \
